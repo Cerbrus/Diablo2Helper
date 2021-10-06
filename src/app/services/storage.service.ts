0975b76825d142043faee5a0d4465247dd35f880
Cerbrus <michiel1900@gmail.com>
2021-10-07 00:35:42 +0200

    Moved setting to settings.json (Fixes #15)

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
+import settings from '../../assets/settings.json';
 import { ArrayHelper } from '../helpers';
 import { IStorage } from '../interfaces/IStorage';
-import { RecordValues } from '../types/helpers';
 import { GetValue, SaveValue } from '../types/storage';
 
 @Injectable({ providedIn: 'root' })
 export class StorageService {
-    private storageKeys: Array<keyof IStorage> = [
+    private readonly defaultValues: IStorage = <IStorage>settings.storageDefaults;
+
+    private readonly storageKeys: Array<keyof IStorage> = [
         'uiActiveTabs',
         'darkMode',
         'runeSort',
@@ -17,46 +19,13 @@ export class StorageService {
         'uiCollapsibleState'
     ];
 
-    private static defaultValues: IStorage = {
-        darkMode: true,
-        runeSort: {
-            name: { key: 'name', icon: 'alpha' },
-            owned: { key: 'owned', icon: 'numericAlt' },
-            number: { key: 'number', icon: 'numeric' }
-        },
-        runeWordFilters: {
-            showUnavailable: true,
-            showCraftable: true,
-            itemTypes: {}
-        },
-        runeWordSort: {
-            name: { key: 'name', icon: 'alpha' },
-            runes: { key: 'runes' },
-            cLvl: { key: 'cLvl', icon: 'numeric' },
-            owned: { key: 'owned', icon: 'numericAlt' }
-        },
-        runeWordsOwned: {},
-        runesOwned: {},
-        uiActiveTabs: {},
-        uiCollapsibleState: {}
-    };
+    public readonly get: GetValue<IStorage> = ArrayHelper
+        .toRecord(this.storageKeys, (key) => <T>(): T => this.getItem(key));
 
-    public get: GetValue<IStorage>;
-    public save: SaveValue<IStorage>;
+    public readonly save: SaveValue<IStorage> = ArrayHelper
+        .toRecord(this.storageKeys, (key) => <T>(value: T): T => this.saveItem(key, value));
 
-    constructor() {
-        this.get = ArrayHelper.toRecord(this.storageKeys,
-            (key) => <T extends RecordValues<IStorage>>(): T =>
-                StorageService.getItem(key)
-        );
-
-        this.save = ArrayHelper.toRecord(this.storageKeys,
-            (key) => <T extends RecordValues<IStorage>>(value: T): T =>
-                StorageService.saveItem(key, value)
-        );
-    }
-
-    private static getItem<T>(key: string): T {
+    private getItem<T>(key: string): T {
         const stored = localStorage.getItem(`d2helper.${key}`);
 
         if (stored === undefined || stored === null) {
@@ -69,7 +38,8 @@ export class StorageService {
         return stored ? JSON.parse(stored) : null;
     }
 
-    private static saveItem<T>(key: string, value: T): T {
+    // noinspection JSMethodCanBeStatic
+    private saveItem<T>(key: string, value: T): T {
         localStorage.setItem(`d2helper.${key}`, JSON.stringify(value));
         return value;
     }
